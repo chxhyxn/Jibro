df94b1084c59f3b60d3ae9367293a4d375438c38
Sean Cho <74293674+chxhyxn@users.noreply.github.com>
2022-03-30 03:49:05 +0900

    init Project

---
+++ PointYouWant/Compass/CompassViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CompassViewController.swift
+//  PointYouWant
+//
+//  Created by chxhyxn on 2022/03/30.
+//
+
+import UIKit
+import CoreLocation
+
+class CompassViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- PointYouWant/Map/MapViewController.swift
+++ PointYouWant/Map/MapViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
