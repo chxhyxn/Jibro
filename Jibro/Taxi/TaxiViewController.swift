b4f62b75b5a9ef3058b91f89dcab38a205270a1e
Sean Cho <74293674+chxhyxn@users.noreply.github.com>
2022-04-06 18:47:37 +0900

    Updating UI...

---
+++ Jibro/Taxi/TaxiViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PointYouWant
+//
+//  Created by chxhyxn on 2022/03/30.
+//
+
+import UIKit
+
+class TaxiViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
